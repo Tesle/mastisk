76c7d53b1df0ef62ecb9e8326ed324d0149bc7a7
unknown <ghosh_su@Subhadip-G.India.XoriantCorp.com>
2016-09-17 03:55:39 +0530

    interaction

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 import core from './module/core';
 import lodash from 'lodash';
+import readline from 'readline';
 
 var _core = new core({value:'test'});
 lodash.map(_core.obj,(value)=>{
     console.log(value);
+});
+
+var rl = readline.createInterface(process.stdin, process.stdout);
+rl.setPrompt('guess> ');
+rl.prompt();
+rl.on('line', function(line) {
+    if (line === "right") rl.close();
+    rl.prompt();
+}).on('close',function(){
+    process.exit(0);
 });
