2e785173f4e5593a340e64e209b766f50ef88ad8
unknown <ghosh_su@Subhadip-G.India.XoriantCorp.com>
2016-11-16 05:10:07 +0530

    logic primary

--- src/app.js
+++ src/app.js
@@ -1,18 +1,47 @@
 import core from './module/core';
 import lodash from 'lodash';
 import readline from 'readline';
+import sizeof from 'sizeof';
+import fs from 'fs';
 
-var _core = new core({value:'test'});
+var _core = new core(255,255,255);
 lodash.map(_core.obj,(value)=>{
     console.log(value);
 });
 
+
 var rl = readline.createInterface(process.stdin, process.stdout);
-rl.setPrompt('guess> ');
+rl.setPrompt('path> ');
 rl.prompt();
 rl.on('line', function(line) {
-    if (line === "right") rl.close();
-    rl.prompt();
+    fs.readFile(line, function(err,data){
+    if (!err){
+     console.log('received data: ('+sizeof.sizeof(data,true)+')' + data);
+     var arr = [];
+     var str = data.toString();
+     for (let i=0; i<=str.length; i=i+3){
+        arr.push( _core.to1D((str[i]||' ').charCodeAt(),(str[i+1]||' ').charCodeAt(),(str[i+2]||' ').charCodeAt()));
+     }
+     console.log("converted to:  ("+sizeof.sizeof(arr,true)+') ->3d' );
+     var resultarr = [];
+     for (let i=0; i<=arr.length; i=i+1){
+         let val = _core.to3D((arr[i]||' '));
+        resultarr.push(val.x);
+         resultarr.push(val.y);
+          resultarr.push(val.z);
+     }
+
+     console.log("again converted to: ("+sizeof.sizeof(resultarr,true)+') ->' + String.fromCharCode(...resultarr));
+      rl.close();
+    }else{
+        console.log(err);
+        rl.prompt();
+    }
+});
+//http://stackoverflow.com/questions/29142417/4d-position-from-1d-index
+    
+    
 }).on('close',function(){
     process.exit(0);
 });
+
--- src/module/core.js
+++ src/module/core.js
@@ -1,7 +1,24 @@
-class core{
-    constructor(obj){
-        this.obj = obj;
-    }    
+class core {
+    constructor(...args) {
+        this.obj = args;
+        this.xMax = args[0];
+        this.yMax = args[1];
+        this.zMax = args[2];
+    }
+    to1D(x, y, z) {
+        console.log(x+','+y+','+z+',');
+        return (z * this.xMax * this.yMax) + (y * this.xMax) + x;
+    }
+
+    to3D(idx) {
+        let z = Math.round( idx / (this.xMax * this.yMax));
+        let idy =idx-( z * this.xMax * this.yMax);
+        let y = Math.round(idy / this.xMax);
+        let x = Math.round(idy % this.xMax);
+        
+        console.log(x+','+y+','+z+',');
+        return { x, y, z };
+    }
 }
 
 export default core;
