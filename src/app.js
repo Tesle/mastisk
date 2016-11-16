871b63a3ddfcc408b22f838aa154df2f022a2bc4
unknown <ghosh_su@Subhadip-G.India.XoriantCorp.com>
2016-11-17 01:16:59 +0530

    dynamic dimention added

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ import readline from 'readline';
 import sizeof from 'sizeof';
 import fs from 'fs';
 
-var _core = new core(255,255,255);
-lodash.map(_core.obj,(value)=>{
+var _core = new core(255, 255, 255);
+var _core4 = new core(255, 255, 255, 255);
+lodash.map(_core.obj, (value) => {
     console.log(value);
 });
 
@@ -13,35 +14,39 @@ lodash.map(_core.obj,(value)=>{
 var rl = readline.createInterface(process.stdin, process.stdout);
 rl.setPrompt('path> ');
 rl.prompt();
-rl.on('line', function(line) {
-    fs.readFile(line, function(err,data){
-    if (!err){
-     console.log('received data: ('+sizeof.sizeof(data,true)+')' + data);
-     var arr = [];
-     var str = data.toString();
-     for (let i=0; i<=str.length; i=i+3){
-        arr.push( _core.to1D((str[i]||' ').charCodeAt(),(str[i+1]||' ').charCodeAt(),(str[i+2]||' ').charCodeAt()));
-     }
-     console.log("converted to:  ("+sizeof.sizeof(arr,true)+') ->3d' );
-     var resultarr = [];
-     for (let i=0; i<=arr.length; i=i+1){
-         let val = _core.to3D((arr[i]||' '));
-        resultarr.push(val.x);
-         resultarr.push(val.y);
-          resultarr.push(val.z);
-     }
+rl.on('line', function (line) {
+    //let line = "c:/log.txt";
+    fs.readFile(line, function (err, data) {
+        if (!err) {
+            console.log('received data: (' + sizeof.sizeof(data, true) + ')' + data);
+            var arr = [];
+            var arr4 =[];
+            var str = data.toString();
+            for (let i = 0; i <= str.length; i = i + 3) {
+                arr.push(_core.to1xD((str[i] || ' ').charCodeAt(), (str[i + 1] || ' ').charCodeAt(), (str[i + 2] || ' ').charCodeAt()));
+            }
+            for (let i = 0; i <= str.length; i = i + 4) {
+                arr4.push(_core4.to1xD((str[i] || ' ').charCodeAt(), (str[i + 1] || ' ').charCodeAt(), (str[i + 2] || ' ').charCodeAt(),(str[i + 3] || ' ').charCodeAt()));
+            }
+            console.log("converted to:  ("+sizeof.sizeof(arr,true)+') ->3d->' +arr);
+            console.log("converted to 4:  ("+sizeof.sizeof(arr4,true)+') ->4d->'+ arr4 );
 
-     console.log("again converted to: ("+sizeof.sizeof(resultarr,true)+') ->' + String.fromCharCode(...resultarr));
-      rl.close();
-    }else{
-        console.log(err);
-        rl.prompt();
-    }
-});
-//http://stackoverflow.com/questions/29142417/4d-position-from-1d-index
-    
-    
-}).on('close',function(){
+            var resultarr = [];
+            for (let i = 0; i <= arr.length; i = i + 1) {
+                let val = _core.toXD((arr[i] || ' '));
+                resultarr.push(...val.reverse());
+            }
+            console.log("again converted to: (" + sizeof.sizeof(resultarr, true) + ') ->' + String.fromCharCode(...resultarr));
+            rl.close();
+        } else {
+            console.log(err);
+            rl.prompt();
+        }
+    });
+    //http://stackoverflow.com/questions/29142417/4d-position-from-1d-index
+
+
+}).on('close', function () {
     process.exit(0);
 });
 
--- src/module/core.js
+++ src/module/core.js
@@ -4,20 +4,64 @@ class core {
         this.xMax = args[0];
         this.yMax = args[1];
         this.zMax = args[2];
+        this.argMax = args;
     }
     to1D(x, y, z) {
-        console.log(x+','+y+','+z+',');
+       // console.log(x + ',' + y + ',' + z + ',');
         return (z * this.xMax * this.yMax) + (y * this.xMax) + x;
     }
 
     to3D(idx) {
-        let z = Math.round( idx / (this.xMax * this.yMax));
-        let idy =idx-( z * this.xMax * this.yMax);
+        let z = Math.round(idx / (this.xMax * this.yMax));
+        let idy = idx - (z * this.xMax * this.yMax);
         let y = Math.round(idy / this.xMax);
         let x = Math.round(idy % this.xMax);
-        
-        console.log(x+','+y+','+z+',');
+
+        //console.log(x + ',' + y + ',' + z + ',');
         return { x, y, z };
+    }
+
+    toXD(indx) {
+        let Xn = [];
+        this.argMax.forEach((x, index) => {
+            let val = ((indx - this.calcDx(Xn)) / this.calcDval(index)) % x;
+            Xn.push(Math.round(val));
+        });
+        return Xn;
+    }
+
+    to1xD(...args) {
+        let xd = args.length;
+        let maxXd = this.argMax.length;
+        let oneD = 0;
+        if (xd !== maxXd) {
+            return [];
+        }
+        args.forEach((x, index) => {
+            oneD = oneD + (x * (this.calcD(index)));
+        });
+        return oneD;
+    }
+    calcD(indx) {
+        let result = 1;
+        this.argMax.forEach((x, index) => {
+            if (index <= indx) {
+                return;
+            }
+            result = x * result;
+        });
+        return result;
+    }
+    calcDx(Xn) {
+        let result = 0;
+        Xn.forEach((x, index) => {
+            result = result + (x * (index !== 0 ? (this.argMax.slice(index - 1, index).reduce((a, b) => a * b)) : 1));
+        });
+        return result;
+    }
+    calcDval(indx) {
+        let result = 1;
+        return indx === 0 ? 1 : this.argMax.slice(0, indx).reduce((a, b) => a * b);
     }
 }
 
